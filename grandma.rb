5b0561b4c492257fae6508d9ebaad7130ab2ef3e
Emily Jones <emjones829@gmail.com>
2020-06-05 01:50:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 # Write a speak_to_grandma method.
 
-speak_to_grandma = "I LOVE YOU GRANDMA!"
+def speak_to_grandma
+  speak_to_grandma = "I LOVE YOU GRANDMA!"
 
 if speak_to_grandma == "HUH?! SPEAK UP, SONNY!"
   puts speak_to_grandma
