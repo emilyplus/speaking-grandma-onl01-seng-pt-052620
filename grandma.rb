f2afa6efff5fcee530faf32fba7103b3db8189a1
Emily Jones <emjones829@gmail.com>
2020-06-05 02:14:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,10 @@ def speak_to_grandma(phrase)
   "I LOVE YOU GRANDMA!"
 if phrase != phrase.upcase
   puts "HUH?! SPEAK UP, SONNY!"
-elsif phrase == phrase.upcase
-  puts "NO, NOT SINCE 1938"
-elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
+elsif phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
+else phrase == phrase.upcase speak_to_grandma 
+  puts "NO, NOT SINCE 1938"
 end
   
 
